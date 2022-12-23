012006a39701bb1c532a996013f648abc5fa7514
Snee <120136010+itssnee@users.noreply.github.com>
2022-12-23 20:32:20 +0100

    Add files via upload

---
+++ data/scripts/tutorial.py
@@ -0,0 +1,33 @@
+import requests
+import time
+import os
+import json
+import datetime
+from datetime import date
+from requests import get, post
+from random import randint
+import subprocess
+import asyncio
+import sys
+import colorama
+from colorama import Fore, Style, init
+import random
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+today = date.today()
+  
+def clearcmd():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def tuto():
+    print("currently being made....")
