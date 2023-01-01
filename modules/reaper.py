76cce1a991cba3ee99d1aafbcfd4b6ce154adb46
Stremz C# <121738120+itsstremz@users.noreply.github.com>
2023-01-02 00:36:52 +0100

    Update reaper.py

--- modules/reaper.py
+++ modules/reaper.py
@@ -1,5 +1,7 @@
 #we know it's not the best version to make a pc fucker, but fon't expect much from a free program
+
 import os
+import pyautogui
 
 lol = input("if you don't want your pc to get fucked, close the program now since te reaper module is build to crash your pc")
 while True:
