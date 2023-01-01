d6d079b42f14c159ae86d8b1a69446e3dd21af20
Stremz C# <121738120+itsstremz@users.noreply.github.com>
2023-01-02 00:35:47 +0100

    Update reaper.py

--- modules/reaper.py
+++ modules/reaper.py
@@ -1,3 +1,4 @@
+#we know it's not the best version to make a pc fucker, but fon't expect much from a free program
 import os
 
 lol = input("if you don't want your pc to get fucked, close the program now since te reaper module is build to crash your pc")
