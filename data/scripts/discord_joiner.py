800127b29655fb93ad4d8da54d1f66b0eae938d2
Snee <120136010+itssnee@users.noreply.github.com>
2023-01-14 22:58:28 +0100

    Update discord_joiner.py

--- data/scripts/discord_joiner.py
+++ data/scripts/discord_joiner.py
@@ -9,16 +9,18 @@ def spammer():
     invite_code = str(requests.get(config["invite"]).text)
     while True:
         sys.stdout.write(colorama.Fore.CYAN + "> ")
-        print("Enter your token: ")
-        tokens = input("")
-        r1 = requests.get('https://discord.com/api/v9/auth/login', headers={"Authorization": tokens})
+        tokens = input("Enter the token you want to join a server with: ")
+        r1 = requests.get('https://discord.com/api/v10/auth/login', headers={"Authorization": tokens})
+        
         if "200" not in str(r1):
             sys.stdout.write(colorama.Fore.RED + "> ")
             print("Invalid Token")
+            
         if "200" in str(r1):
-            r = requests.get(f'https://discord.com/api/v9/invite/{invite_code}', headers={"Authorization": tokens})
+            r = requests.get(f'https://discord.com/api/v10/invite/{invite_code}', headers={"Authorization": tokens})
             if "200" in str(r):
                 break
+                  
             if "403" in str(r):
                 sys.stdout.write(colorama.Fore.RED + "> ")
                 print("Locked Token")
